f5f8e2143878a2eb36277544802b936e066b0384
MUbarak123-56 <41446936+MUbarak123-56@users.noreply.github.com>
2018-07-26 13:23:20 -0500

    Update config.default.js

--- recipes/speech_to_text/config.default.js
+++ recipes/speech_to_text/config.default.js
@@ -3,12 +3,20 @@
 * IMPORTANT NOTES:
 *  Please ensure you do not interchange your username and password.
 *  Your username is the longer value: 36 digits, including hyphens
-*  Your password is the smaller value: 12 characters
+*  Your password is the smaller value: 12 character{
+  "url": "https://stream.watsonplatform.net/speech-to-text/api",
+  "username": "0c7a9279-8534-4a88-8ddc-bb28be4c85b3",
+  "password": "inPUxHIscoBH"
+}s
 */
 
 // Create the credentials object for export
-exports.credentials = {};
-
+exports.credentials = {{
+  "url": "https://stream.watsonplatform.net/speech-to-text/api",
+  "username": "0c7a9279-8534-4a88-8ddc-bb28be4c85b3",
+  "password": "inPUxHIscoBH"
+};
+}
 // Watson Speech to Text
 // https://www.ibm.com/watson/services/speech-to-text/
 exports.credentials.speech_to_text = {
